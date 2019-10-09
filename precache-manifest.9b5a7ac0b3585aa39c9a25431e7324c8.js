908ba3cc8a461550a818f1c522fb57ee79418a0d
Jampa Thinlay <jampathinlay@Jampas-MacBook-Pro.local>
2019-10-09 15:01:58 -0700

    Updates

---
+++ precache-manifest.9b5a7ac0b3585aa39c9a25431e7324c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2256a639d35d1f35fa57498cbf8ac573",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c74c84823364a8ef035b",
+    "url": "/monsters-rolodex/static/css/main.a1887680.chunk.css"
+  },
+  {
+    "revision": "db5eea2cd8591c1329b8",
+    "url": "/monsters-rolodex/static/js/2.81e14308.chunk.js"
+  },
+  {
+    "revision": "c74c84823364a8ef035b",
+    "url": "/monsters-rolodex/static/js/main.042bfeab.chunk.js"
+  },
+  {
+    "revision": "42280f1e7ca649a3b03a",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1619bbd.js"
+  }
+]);
